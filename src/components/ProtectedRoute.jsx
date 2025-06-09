d408ddb17418dbadb44757708a9d2708b81df652
Peter Neufeld <pneufeld@sonderpreis-baumarkt.de>
2025-06-09 22:26:01 +0200

    🔐 ProtectedRoute auf sessionStorage angepasst

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }) {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(sessionStorage.getItem('user')); // ⬅️ wurde angepasst!
   if (!user) return <Navigate to="/" />;
   return children;
 }
