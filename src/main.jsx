6c075ccc955ff4f650311cee5f78394628bdd9fe
Peter Neufeld <pneufeld@sonderpreis-baumarkt.de>
2026-01-06 12:34:14 +0100

    ✨ Toaster (fronend) - main.jsx – Version 1.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { Toaster } from 'react-hot-toast';
+
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+
+      {/* Globaler Toaster (muss im Root gemountet sein, sonst erscheinen keine Toasts) */}
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={10}
+        containerStyle={{ zIndex: 999999 }}
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
